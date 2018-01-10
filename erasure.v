ea7c2c2d97d73533aad51a99fbec3e55645878b7
Amin Timany <amintimany@gmail.com>
2018-01-10 15:08:19 +0100

    Prove erasure

---
+++ erasure.v
@@ -0,0 +1,140 @@
+From iris.program_logic Require Export language ectx_language ectxi_language.
+From iris_io.prelude Require Export base.
+From iris_io Require Export lang lang_erased.
+From iris.algebra Require Export ofe.
+From stdpp Require Import gmap.
+
+Definition have_same_proph
+           (σ : language.state P_lang) (σ' : language.state PE_lang)
+  := σ.1 = σ'.1 ∧ dom (gset loc) σ.2 = σ'.2.
+
+Definition conjure_prophecies (σ : language.state PE_lang) :
+  language.state P_lang :=
+  match σ with
+    (σh, σp) => (σh,  to_gmap (Sconst UnitV) σp)
+  end.
+
+Lemma conjure_prophecies_have_same_proph σ :
+  have_same_proph (conjure_prophecies σ) σ.
+Proof.
+  destruct σ; split; first done; simpl.
+  apply mapset.mapset_eq => x; split => Hx.
+  - destruct (decide (x ∈ m)); first done.
+    apply elem_of_dom in Hx. destruct Hx as [y Hy].
+    rewrite lookup_to_gmap option_guard_False //= in Hy.
+  - apply elem_of_dom. rewrite lookup_to_gmap option_guard_True; eauto.
+Qed.
+
+Lemma erased_reachable_reachable th1 h1 th2 σ2:
+  rtc (@step PE_lang) (th1, (h1, ∅)) (th2, σ2) →
+  ∀ σ2', have_same_proph σ2' σ2 →
+         rtc (@step P_lang) (th1, (h1, ∅)) (th2, σ2').
+Proof.
+  remember (th1, (h1, ∅)) as cfg.
+  remember (th2, σ2) as cfg'.
+  intros Hrtc.
+  revert th1 h1 th2 σ2 Heqcfg Heqcfg'.
+  eapply (rtc_ind_r_weak (λ z z', ∀ (th1 : list (language.expr PE_lang)) (h1 : gmap loc val)
+                                    (th2 : list (language.expr PE_lang))
+                                    (σ2 : language.state PE_lang),
+                             z = (th1, (h1, ∅))
+                             → z' = (th2, σ2) →
+                             ∀ σ2' : language.state P_lang,
+                               have_same_proph σ2' σ2 →
+                               rtc (@step P_lang) (th1, (h1, ∅)) (th2, σ2')));
+  last eauto.
+  - intros x th1 h1 th2 σ2 Heqcfg Heqcfg'; simplify_eq.
+    intros [σ2 σ2p] [Hh Hsp]; simpl in *.
+    rewrite Hh. rewrite (dom_empty_inv_L (D := gset loc) σ2p); last done.
+    econstructor.
+  - intros x y z Hxy Hyz IH th1 h1 th2 σ2 Heqcfg Heqcfg' σ2' Hsp;
+      simplify_eq; simpl in *.
+    destruct y as [yh yp].
+    inversion Hyz as [? ? ? ? ? ? ? ? ? Hpr]; simplify_eq.
+    inversion Hpr as [? ? ? ? ? Hestp]; simpl in *; simplify_eq.
+    inversion Hestp; simplify_eq;
+      try (eapply rtc_r; first (by eapply IH; eauto); by repeat (econstructor; eauto)).
+    all: destruct σ2' as [σ2'h σ2'p]; destruct Hsp as [Hsp1 Hsp2]; simpl in *; simplify_eq.
+    + eapply (rtc_r _ (_ ++ fill K (Alloc _) :: _, (σ, σ2'p)));
+        [unshelve eapply (IH _ _ _ (_, _) _ _ (σ, σ2'p)); eauto; split; simpl; eauto|
+         repeat (econstructor; eauto)].
+    + match goal with A : ?X.1 !! _ = _ |- _ => destruct X as [σmh σmp]; simpl in * end.
+      eapply (rtc_r _ (_ ++ fill K (Load (Loc _)) :: _, (_, σ2'p)));
+        [unshelve eapply (IH _ _ _ (σmh, σmp) _ _ (_, σ2'p)); eauto; split; simpl; eauto|
+         repeat (econstructor; eauto)].
+    + eapply (rtc_r _ (_ ++ fill K (Store (Loc _) _) :: _, (_, σ2'p)));
+        [unshelve eapply (IH _ _ _ (_, _) _ _ (_, σ2'p)); eauto; split; simpl; eauto|
+         repeat (econstructor; eauto)].
+    + match goal with A : ?X.1 !! _ = _ |- _ => destruct X as [σmh σmp]; simpl in * end.
+      eapply (rtc_r _ (_ ++ fill K (CAS (Loc _) _ _) :: _, (_, σ2'p)));
+        [unshelve eapply (IH _ _ _ (_, _) _ _ (_, σ2'p)); eauto; split; simpl; eauto|
+         repeat (econstructor; eauto)].
+    + eapply (rtc_r _ (_ ++ fill K (CAS (Loc _) _ _) :: _, (_, σ2'p)));
+        [unshelve eapply (IH _ _ _ (_, _) _ _ (_, σ2'p)); eauto; split; simpl; eauto|
+         repeat (econstructor; eauto)].
+    + assert (({[fresh σp]} ∪ σp) ∖ {[fresh σp]} = σp) as Hfr.
+      { rewrite difference_union_distr_l_L difference_diag_L left_id_L.
+        apply difference_disjoint_L.
+        symmetry. apply disjoint_singleton_l, is_fresh. }
+      eapply (rtc_r _ (_ ++ fill K (Create_Pr) :: _, (_, delete (fresh σp) σ2'p)));
+        [unshelve eapply (IH _ _ _ (_, _) _ _ (_, _)); eauto; split; simpl; eauto|
+         repeat (econstructor; eauto)].
+      * by rewrite (dom_delete_L (D := gset loc)) Hsp2 Hfr.
+      * destruct (elem_of_dom (D := gset loc) σ2'p (fresh σp)) as [[str Hstr]].
+        { rewrite Hsp2. by apply elem_of_union_l, elem_of_singleton. }
+        replace σ2'p with (<[fresh (dom (gset loc) (delete (fresh σp) σ2'p)) := str]> (delete (fresh σp) σ2'p)) at 2; last first.
+        { rewrite dom_delete_L Hsp2 Hfr insert_delete. by apply insert_id. }
+        rewrite -{2}Hfr -Hsp2 -dom_delete_L.
+        econstructor.
+    + assert (∃ str, σ2'p !! l = Some str) as [str Hstr]
+          by by apply (elem_of_dom (D := gset loc)).
+      eapply (rtc_r _ (_ ++ fill K (Assign_Pr (Pr _) _) :: _,
+                       (_, <[l:={| Shead := v; Stail := str|}]> σ2'p)));
+        [unshelve eapply (IH _ _ _ (_, _) _ _ (_, _)); eauto; split; simpl; eauto|
+         repeat (econstructor; eauto)].
+      * rewrite dom_insert_L. by apply subseteq_union_1_L, elem_of_subseteq_singleton.
+      * replace σ2'p with (<[l:= Stail {| Shead := v; Stail := str |}]>(<[l:={| Shead := v; Stail := str |}]> σ2'p)) at 2; last first.
+        { rewrite /= insert_insert. by apply insert_id. }
+        econstructor; auto.
+        rewrite lookup_insert //.
+Qed.
+
+Definition safe e :=
+  ∀ th2 σ2,
+    rtc (@step P_lang) ([e], (∅, ∅)) (th2, σ2) →
+    ∀ e, e ∈ th2 → AsVal e ∨ reducible e σ2.
+
+Definition erased_safe e :=
+  ∀ th2 σ2,
+    rtc (@step PE_lang) ([e], (∅, ∅)) (th2, σ2) →
+    ∀ e, e ∈ th2 → AsVal e ∨ reducible e σ2.
+
+Lemma reducible_erase_reducible e σ σ' :
+  have_same_proph σ σ' →
+  reducible e σ →
+  @language.reducible PE_lang e σ'.
+Proof.
+  intros Hsp (e'&σ2&efs&Hrd); simpl in *.
+  inversion Hrd as [K e1' e2' ? ? Hhrd]; simpl in *; subst.
+  destruct σ as [σh σp]; destruct σ' as [σ'h σ'p];
+    destruct Hsp as [? ?]; simpl in *; simplify_eq.
+  inversion Hhrd; subst; try (repeat econstructor; eauto; fail).
+  + match goal with A : is_Some _ |- _ => destruct A as [? ?] end.
+    repeat econstructor; eauto.
+  + repeat econstructor; eauto.
+    apply elem_of_dom; eauto.
+  + repeat econstructor; eauto.
+    apply elem_of_dom; eauto.
+Qed.
+
+Lemma soundness_prophecies e :
+  safe e → erased_safe e.
+Proof.
+  intros Hs th2 σ2 Hrtc re Hre.
+  assert (rtc step ([e], (∅, ∅)) (th2, conjure_prophecies σ2)) as Hrtc'.
+  { eapply erased_reachable_reachable;
+      eauto using conjure_prophecies_have_same_proph. }
+  edestruct Hs as [?|Hred]; eauto.
+  right. eapply reducible_erase_reducible;
+           eauto using conjure_prophecies_have_same_proph.
+Qed.
---
+++ lang_erased.v
@@ -0,0 +1,108 @@
+From iris.program_logic Require Export language ectx_language ectxi_language.
+From iris_io.prelude Require Export base.
+From iris_io Require Export lang.
+From iris.algebra Require Export ofe.
+From stdpp Require Import gmap.
+
+Module Plang_erased.
+
+  Definition erased_state : Type := (gmap loc val) * (gset loc).
+
+  Inductive erased_head_step :
+    expr → erased_state → expr → erased_state → list expr → Prop :=
+  (* β *)
+  | EBetaS e1 e2 v2 σ :
+      to_val e2 = Some v2 →
+      erased_head_step (App (Rec e1) e2) σ e1.[(Rec e1), e2/] σ []
+  (* Products *)
+  | EFstS e1 v1 e2 v2 σ :
+      to_val e1 = Some v1 → to_val e2 = Some v2 →
+      erased_head_step (Fst (Pair e1 e2)) σ e1 σ []
+  | ESndS e1 v1 e2 v2 σ :
+      to_val e1 = Some v1 → to_val e2 = Some v2 →
+      erased_head_step (Snd (Pair e1 e2)) σ e2 σ []
+  (* Sums *)
+  | ECaseLS e0 v0 e1 e2 σ :
+      to_val e0 = Some v0 →
+      erased_head_step (Case (InjL e0) e1 e2) σ e1.[e0/] σ []
+  | ECaseRS e0 v0 e1 e2 σ :
+      to_val e0 = Some v0 →
+      erased_head_step (Case (InjR e0) e1 e2) σ e2.[e0/] σ []
+    (* nat bin op *)
+  | EBinOpS op a b σ :
+      erased_head_step (BinOp op (#n a) (#n b)) σ (of_val (binop_eval op a b)) σ []
+  (* If then else *)
+  | EIfFalse e1 e2 σ :
+      erased_head_step (If (#♭ false) e1 e2) σ e2 σ []
+  | EIfTrue e1 e2 σ :
+      erased_head_step (If (#♭ true) e1 e2) σ e1 σ []
+  (* Recursive Types *)
+  | EUnfold_Fold e v σ :
+      to_val e = Some v →
+      erased_head_step (Unfold (Fold e)) σ e σ []
+  (* Polymorphic Types *)
+  | ETBeta e σ :
+      erased_head_step (TApp (TLam e)) σ e σ []
+  (* Concurrency *)
+  | EForkS e σ:
+      erased_head_step (Fork e) σ Unit σ [e]
+  (* Reference Types *)
+  | EAllocS e v σ σp l :
+     to_val e = Some v → σ !! l = None →
+     erased_head_step (Alloc e) (σ, σp) (Loc l) (<[l:=v]>σ, σp) []
+  | ELoadS l v σ :
+     σ.1 !! l = Some v →
+     erased_head_step (Load (Loc l)) σ (of_val v) σ []
+  | EStoreS l e v σ σp :
+     to_val e = Some v → is_Some (σ !! l) →
+     erased_head_step (Store (Loc l) e) (σ, σp) Unit (<[l:=v]>σ, σp) []
+  (* Compare and swap *)
+  | ECasFailS l e1 v1 e2 v2 vl σ :
+     to_val e1 = Some v1 → to_val e2 = Some v2 →
+     σ.1 !! l = Some vl → vl ≠ v1 →
+     erased_head_step (CAS (Loc l) e1 e2) σ (#♭ false) σ []
+  | ECasSucS l e1 v1 e2 v2 σ σp :
+     to_val e1 = Some v1 → to_val e2 = Some v2 →
+     σ !! l = Some v1 →
+     erased_head_step (CAS (Loc l) e1 e2) (σ, σp) (#♭ true) (<[l:=v2]>σ, σp) []
+  (* Prophecy operational semantics *)
+  | ECreate_PrS σ σp :
+      erased_head_step Create_Pr (σ, σp) (Pr (fresh σp))
+                (σ, {[fresh σp]} ∪ σp) []
+  | EAssignS v l e σ σp :
+     l ∈ σp → to_val e = Some v →
+     erased_head_step (Assign_Pr (Pr l) e) (σ, σp) Unit (σ, σp) [].
+
+  (** Basic properties about the language *)
+  Lemma val_stuck e1 σ1 e2 σ2 ef :
+    erased_head_step e1 σ1 e2 σ2 ef → to_val e1 = None.
+  Proof. destruct 1; naive_solver. Qed.
+
+  Lemma head_ctx_step_val Ki e σ1 e2 σ2 ef :
+    erased_head_step (fill_item Ki e) σ1 e2 σ2 ef → is_Some (to_val e).
+  Proof. destruct Ki; inversion_clear 1; simplify_option_eq; eauto. Qed.
+
+  Lemma alloc_fresh e v σ σp :
+    let l := fresh (dom (gset loc) σ) in
+    to_val e = Some v → erased_head_step (Alloc e) (σ, σp) (Loc l) (<[l:=v]>σ, σp) [].
+  Proof. by intros; apply EAllocS, (not_elem_of_dom (D:=gset loc)), is_fresh. Qed.
+
+  Lemma val_head_stuck e1 σ1 e2 σ2 efs : erased_head_step e1 σ1 e2 σ2 efs → to_val e1 = None.
+  Proof. destruct 1; naive_solver. Qed.
+
+  Lemma lang_mixin : EctxiLanguageMixin of_val to_val fill_item erased_head_step.
+  Proof.
+    split; apply _ || eauto using to_of_val, of_to_val, val_head_stuck,
+           fill_item_val, fill_item_no_val_inj, head_ctx_step_val.
+  Qed.
+
+End Plang_erased.
+
+Canonical Structure PE_ectxi_lang :=
+  EctxiLanguage Plang_erased.lang_mixin.
+Canonical Structure PE_ectx_lang :=
+  EctxLanguageOfEctxi PE_ectxi_lang.
+Canonical Structure PE_lang :=
+  LanguageOfEctx PE_ectx_lang.
+
+Export Plang_erased.
