8ca08c674939e3769e75b480e7b49fb3a27aa164
Amin Timany <amintimany@gmail.com>
2018-02-15 09:58:10 +0100

    Adapt the coin proof

--- coin.v
+++ coin.v
@@ -1,129 +1,32 @@
 From iris.program_logic Require Export weakestpre lifting.
 From iris.proofmode Require Import tactics.
 From iris_io Require Import lang rules.
+Require Import Coq.Logic.PropExtensionality.
 Require Import Coq.Logic.FunctionalExtensionality.
 
+Definition coin_proph (s : Stream val) :=
+  ∀ n, ∃ v, Snth n s = #♭v v.
 
-CoFixpoint str_boolV (s : Stream bool) :=
-  {| Shead := BoolV (Shead s); Stail := str_boolV (Stail s) |}.
-
-CoFixpoint str_bool (s : Stream bool) :=
-  {| Shead := Bool (Shead s); Stail := str_bool (Stail s) |}.
-
-Lemma str_bool_vals (x : Stream bool) :
-  ∃ s : Stream val, proph_to_expr s = str_bool x.
+Lemma coin_proph_after_read_eq b : with_head coin_proph (#♭v b) = coin_proph.
 Proof.
-  exists (str_boolV x).
-  apply Seq_eq.
-  revert x. cofix IH => x.
-  rewrite [str_bool _]Stream_unfold; rewrite [proph_to_expr _]Stream_unfold.
-  destruct x; simpl.
-  econstructor; auto.
+  extensionality s.
+  apply propositional_extensionality; split.
+  - intros Hr n; destruct (Hr (S n)) as [p Hp]; simpl in *; eauto.
+  - intros Hc n. destruct n as [|n]; first eauto.
+    destruct (Hc n) as [p Hp]; eauto.
 Qed.
 
-Lemma str_bool_ent (n : nat) (s s' : Stream val) :
-    Elem (proph_to_expr s) str_bool →
-    Elem (proph_to_expr s') str_bool →
-    Elem (proph_to_expr (take_nth_from n s s')) str_bool.
+Lemma coin_proph_inh : ∃ s : Stream val, coin_proph s.
 Proof.
-  intros [x Hx] [y Hy].
-  exists (take_nth_from n x y).
-  revert s s' x y Hx Hy; induction n => s s' x y.
-  - destruct x as [xh xt]; destruct y as [yh yt];
-      destruct s as [sh st]; destruct s' as [s'h s't]; simpl in *.
-    rewrite [str_bool _]Stream_unfold [proph_to_expr _]Stream_unfold => Hx.
-    rewrite [str_bool _]Stream_unfold [proph_to_expr _]Stream_unfold => Hy.
-    rewrite [str_bool _]Stream_unfold [proph_to_expr _]Stream_unfold.
-    simpl in *.
-    by inversion Hx; inversion Hy; simplify_eq.
-  - destruct x as [xh xt]; destruct y as [yh yt];
-      destruct s as [sh st]; destruct s' as [s'h s't]; simpl in *.
-    rewrite [str_bool _]Stream_unfold [proph_to_expr _]Stream_unfold => Hx.
-    rewrite [str_bool _]Stream_unfold [proph_to_expr _]Stream_unfold => Hy.
-    rewrite [str_bool _]Stream_unfold [proph_to_expr _]Stream_unfold.
-    simpl in *.
-    inversion Hx; inversion Hy; simplify_eq.
-    f_equal; auto.
-Qed.
-
-Lemma after_nth_const n b :
-  Nat.iter n Stail (str_bool (Sconst b)) = Sconst (#♭ b).
-Proof.
-  induction n.
-  - simpl.
-    apply Seq_eq.
-    cofix IH.
-    rewrite [Sconst (#♭ _)]Stream_unfold [str_bool _]Stream_unfold; simpl.
-    econstructor; auto.
-  - by simpl; rewrite IHn.
-Qed.
-
-Lemma str_bool_inh : ∃ p : Stream val, Elem (proph_to_expr p) str_bool.
-Proof.
-  exists (Sconst (BoolV true)), (Sconst true).
-  etrans; first apply (after_nth_const 0 true).
-  apply Seq_eq.
-  cofix IH.
-  rewrite [Sconst _]Stream_unfold; rewrite [proph_to_expr _]Stream_unfold; simpl.
-  econstructor; auto.
-Qed.
-
-Definition proph_after_nth_toss p n :=
-  PrP_eq (PrA p) (PrP p) (Stream bool) (λ x, Nat.iter n Stail (str_bool x)).
-
-Definition can_toss p b :=
-  ∃ z s, (PrP p z = s) ∧ (Shead s = (Bool b)).
-
-Lemma can_toss_after_nth p n b :
-  proph_after_nth_toss p n → can_toss p b.
-Proof.
-  intros [Heq1 Heq2].
-  destruct p as [pS pA pP pPv pPe pPSI]; simpl in *.
-  subst.
-  exists (Sconst b). exists (Sconst (Bool b)); simpl; split; auto.
-  rewrite Heq2.  apply after_nth_const.
-Qed.
-
-Lemma head_after_nth_toos p n :
-  proph_after_nth_toss p n → ∃ b, Shead (PrS p) = BoolV b.
-Proof.
-  intros [Heq1 Heq2].
-  destruct p as [pS pA pP pPv pPe pPSI]; simpl in *.
-  subst.
-  destruct pPSI as [x Hx].
-  destruct (pPv x) as [u Hu].
-  exists (Shead (Nat.iter n Stail x)).
-  rewrite Heq2 in Hx.
-  clear -Hx.
-  revert pS x Hx.
-  induction n => pS x Hx; simpl in *.
-  - destruct pS as [h t]; simpl in *.
-    apply (f_equal Shead) in Hx; simpl in *.
-    apply (f_equal to_val) in Hx; simpl in *.
-    by rewrite to_of_val in Hx; simplify_eq.
-  - rewrite -Nat_iter_S Nat_iter_S_r in Hx.
-    destruct x as [xh xt]; simpl in *.
-    apply IHn in Hx. rewrite Hx.
-    by rewrite -Nat_iter_S Nat_iter_S_r.
-Qed.
-
-Lemma proph_after_Snth_toss_tail p n :
-  proph_after_nth_toss p n →
-  proph_after_nth_toss (Proph_tail p) (S n).
-Proof.
-  intros [Heq1 Heq2].
-  destruct p as [pS pA pP pPv pPe pPSI]; simpl in *.
-  subst.
-  unshelve econstructor; eauto; simpl.
-  extensionality z.
-  by rewrite Heq2.
+  exists (Sconst (#♭v true)).
+  intros n; induction n; eauto.
 Qed.
 
 Section coin.
   Context `{heapIG Σ}.
 
   Definition make_coin :=
-    Rec (Pair (Alloc (InjR Plang.Unit)) (Create_Pr _ str_bool)).
+    Rec (Pair (Alloc (InjR Plang.Unit)) Create_Pr).
 
   Definition flip :=
     Rec (Rec ((Store (Var 3) (InjR Plang.Unit)))).
@@ -143,8 +46,8 @@ Section coin.
                Rand))).
 
   Definition Coin (b : bool) (c l : loc) : iProp Σ :=
-    (∃ p, l ↦ₚ p ∗ ⌜ ∃ n, proph_after_nth_toss p n ⌝ ∗
-            (c ↦ (InjLV (BoolV b)) ∨ (c ↦ (InjRV UnitV)) ∗ ⌜Shead (PrS p) = BoolV b⌝))%I.
+    (∃ p, cpvar l coin_proph p ∗
+            (c ↦ (InjLV (BoolV b)) ∨ ((c ↦ (InjRV UnitV)) ∗ ⌜Shead p = #♭v b⌝)))%I.
 
   Lemma wp_make_coin :
     {{{ True }}}
@@ -158,15 +61,14 @@ Section coin.
     iApply wp_alloc; auto.
     iNext. iIntros (c) "Hc"; simpl.
     iApply (wp_bind (fill [PairRCtx (LocV _)])).
-    iApply wp_create_pr; auto using str_bool_vals, str_bool_ent, str_bool_inh.
-    iNext. iIntros (l) "Hl"; simpl. iDestruct "Hl" as (p) "[Hl Heq]".
-    iDestruct "Heq" as %Heq.
+    iApply (wp_create_pr _ coin_proph); auto using coin_proph_inh.
+    iNext. iIntros (l) "Hl"; simpl. iDestruct "Hl" as (p) "Hl".
     iApply wp_value. iApply "HΦ".
-    destruct (PrSI p) as [y Hy].
-    destruct (head_after_nth_toos p 0) as [b Hb]; auto.
-    iExists b.
-    iExists p; iFrame. iSplit; auto.
-    { iPureIntro. exists 0; simpl; auto. }
+    iDestruct (cpvar_accepted with "Hl") as "#Hac".
+    iDestruct (accepts_head with "Hac") as %[μ Hμ].
+    destruct (Hμ 0) as [b Hb].
+    iExists b, p; iFrame. iRight; iFrame.
+    { by iPureIntro. }
   Qed.
 
   Lemma wp_flip b c l :
@@ -177,20 +79,19 @@ Section coin.
     iApply wp_pure_step_later; auto; iNext. asimpl.
     iApply wp_value.
     iApply wp_pure_step_later; auto; iNext. asimpl.
-    iDestruct "HC" as (p) "(Hl & HP & Hc)".
-    iDestruct "HP" as %[n HP].
-    pose proof (can_toss_after_nth _ n true HP).
+    iDestruct "HC" as (p) "(Hl & Hc)".
     iDestruct "Hc" as "[Hc|[Hc Hh]]".
     - iApply (wp_store with "[$Hc]"). iNext. iIntros "Hc".
-      destruct (head_after_nth_toos p n) as [r Hr]; auto.
-      iApply "HΦ". iExists r.
-      iExists p; iFrame.
-      iSplit; eauto.
+      iApply "HΦ".
+      iDestruct (cpvar_accepted with "Hl") as "#Hac".
+      iDestruct (accepts_head with "Hac") as %[μ Hμ].
+      destruct (Hμ 0) as [r Hr].
+      iExists r, p; iFrame. iRight; iFrame.
+      { by iPureIntro. }
     - iDestruct "Hh" as %Hh.
       iApply (wp_store with "[$Hc]"). iNext. iIntros "Hc".
       iApply "HΦ". iExists b.
-      iExists p; iFrame.
-      iSplit; eauto.
+      iExists p; iFrame. iRight; eauto.
   Qed.
 
   Lemma wp_read b c l :
@@ -201,8 +102,7 @@ Section coin.
     iApply wp_pure_step_later; auto; iNext. asimpl.
     iApply wp_value.
     iApply wp_pure_step_later; auto; iNext. asimpl.
-    iDestruct "HC" as (p) "(Hl & HP & Hc)".
-    iDestruct "HP" as %[n HP].
+    iDestruct "HC" as (p) "(Hl & Hc)".
     iDestruct "Hc" as "[Hc|[Hc Hh]]".
     - iApply (wp_bind (fill [CaseCtx _ _])).
       iApply (wp_load with "[$Hc]"). iNext. iIntros "Hc". simpl.
@@ -210,7 +110,7 @@ Section coin.
       iApply wp_value.
       iApply "HΦ".
       iExists p; iFrame.
-      iSplit; eauto.
+      iLeft; auto.
     - iDestruct "Hh" as %Hh.
       iApply (wp_bind (fill [CaseCtx _ _])).
       iApply (wp_load with "[$Hc]"). iNext. iIntros "Hc". simpl.
@@ -223,17 +123,17 @@ Section coin.
       iApply wp_pure_step_later; auto; iNext. asimpl.
       iApply (wp_bind (fill [AppRCtx (RecV _)])).
       iApply (wp_assign_pr with "[$Hl]").
-      { destruct (can_toss_after_nth p n r) as (z & s & Hz & Hs); eauto. }
+      { iExists (Sconst (#♭v r)). iApply accept_accepts.
+        intros n; exists r. destruct n; first done. apply (Snth_Sconst (S n)). }
       iNext. iIntros "[HP Hl]". iDestruct "HP" as %HnP.
       simpl.
       iApply wp_pure_step_later; auto; iNext. asimpl.
       iApply wp_value.
       rewrite Hh in HnP. simplify_eq.
       iApply "HΦ".
+      rewrite coin_proph_after_read_eq.
       iExists _; iFrame.
-      iSplit; eauto.
-      iPureIntro.
-      exists (S n). by apply proph_after_Snth_toss_tail.
+      by iLeft.
   Qed.
 
 End coin.
